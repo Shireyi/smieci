57980f27466e99505887163786da210f5d199870
Weronika Stolarczyk <majonezowa.stolarczyk@gmail.com>
2018-03-30 13:00:15 +0200

    komit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int x)
+{
+    if (x==0){
+        return 0;
+    }
+    if (x==1){
+         return 1;
+    }
+    if (x>=2){
+        return fib(x-1)+fib(x-2);
+    }
+}
+
+int main()
+{
+    int index;
+    cin >> index;
+    cout << fib(index);
+}
